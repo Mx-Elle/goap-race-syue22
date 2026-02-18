aef280f1cfb9181085dd90f73c628b7f817fd29e
syue22 <syue@exeter.edu>
2026-02-18 08:16:25 -0500

    update

--- serena_bot.py
+++ serena_bot.py
@@ -1,16 +1,19 @@
+import heapq
+import copy
 from game_world.racetrack import RaceTrack
 from collections import defaultdict
-import heapq
+from collections import deque
+from copy import deepcopy
 
 Point = tuple[int, int]
 
 def manhattan_dist(a: Point, b: Point) -> int:
-        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 def find_valid_neighbors(a: Point):
-        neighbors = [(a[0] - 1, a[1]), (a[0], a[1] - 1), (a[0] + 1, a[1]), (a[0], a[1] + 1)]
-        return neighbors
+    neighbors = [(a[0] - 1, a[1]), (a[0], a[1] - 1), (a[0] + 1, a[1]), (a[0], a[1] + 1)]
+    return neighbors
 
 class State:
 
@@ -19,33 +22,39 @@ class State:
         self.track = track
         self.buttons = dict()
         self.pos = track.spawn
-        self.states = []
+        self.buttons = {} # color, bool (reachable)
+        self.doors = {}
+        self.children = [track]
 
 
-    def bfs(self):
-        can_visit = []
-        visited = []
-        current = self.pos
-        for neighbor in find_valid_neighbors(current):
-            if neighbor in self.track.find_traversable_cells() and self.astar(self.loc, neighbor) and neighbor not in visited:
-                can_visit.append(neighbor)
-                current = neighbor
-
-    def state(self: Point):
-        # make new states. loop. check if state is new. if so, add to list.
-        not_visited = set()
-        if self.astar(self.loc, self.track.target):
-            return self.astar(self.loc, self.track.target)
-        else:
-            while not_visited:
-                for neighbor in find_valid_neighbors()
+    def bfs(self, child): #finds all visitable cells
+        frontier = deque([self.pos])
+        visited = set([self.pos])
         
-        # for children, if state is different, add
+        while frontier:
+            current = frontier.popleft()
 
-        # new_doors = []
-        # new_buttons = []
-        # if new_doors != doors or new_buttons != buttons or new_agent != agent:
-        #     states.append(current_state)
+            for neighbor in find_valid_neighbors(current):
+                if (neighbor in self.track.find_traversable_cells() and neighbor not in visited):
+                    visited.add(neighbor)
+                    frontier.append(neighbor)
+        return visited
+
+
+    def make_move(self, button):
+        child = copy.deepcopy(self.track)
+        child.toggle(button)
+        return child
+
+
+    def state(self):
+        # make new states. loop. check if state is new. if so, add to list.
+        for button in self.buttons.values():
+            if button == True:
+                new_state = self.make_move(button)
+                self.children.append(new_state)
+                
+        
 
     def astar(self, start: Point, end: Point) -> list[Point] | None:
         start_cell, end_cell = start, end
@@ -91,8 +100,12 @@ class State:
 
 
     def smart_move(self) -> Point:
-        # if move not illegal
-        path = self.astar(self.pos, self.track.target)
-        # self.track.target
-        next_pos = path[1]
-        return (next_pos[0] - self.pos[0], next_pos[1] - self.pos[1])
+        # path = self.astar(self.pos, self.track.target)
+        # next_pos = path[1]
+        # return (next_pos[0] - self.pos[0], next_pos[1] - self.pos[1])
+        for button in self.track.buttons:
+            if button in self.bfs():
+                self.buttons[button.color] = True
+        for door in self.track.doors:
+            if door in self.bfs():
+                self.doors[door.color] = True
